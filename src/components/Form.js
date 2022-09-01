0835531a0f5fd27ce2061135ef0754a472d01390
nabanichowdhury <nabanichowdhury@gmail.com>
2022-09-01 15:02:28 +0530

    second commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,74 +1,123 @@
 import { faCheckCircle, faEye, faEyeSlash, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { useForm } from 'react-hook-form';
 import './Form.css';
 
 
 
 const Form = () => {
-    const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
-  //handle if valid
-  const valid=(item,v_icon,inv_icon)=>{
-    let text=document.querySelector(`#${item}`)
-    text.style.color='green';
+  
+ const initialState={username:'',email:'',password:''}
+ const [formValues,setFormValues]=useState(initialState)
+ const [formErrors,setFormErrors]=useState({})
+ const [isSubmit,setSubmit]=useState(false)
+ const[isValidPassword,setValidPassword]=useState(false)
+ const[isValidCapital,setValidCapital]=useState(false)
+ const[isValidSmall,setValidSmall]=useState(false)
+ const[isValidPasswordlength,setValidPasswordlength]=useState(false)
+ const[isValidSpecial,setValidSpecial]=useState(false)
+ const[isValidNumber,setValidNumber]=useState(false)
 
-    let valid_icon=document.querySelector(`#${item}.${v_icon}`)
-    // valid_icon.style.color='green';
+ useEffect(()=>{
+    console.log(formErrors)
+    if(Object.keys(formErrors).length===0 && isSubmit){
+        console.log(formValues)
+    }
 
-    let invalid_icon=document.querySelector(`#${item}.${inv_icon}`)
-    invalid_icon.style.opacity='0'
-
-  }
-  //handle if invalid
-  const invalid=(item,v_icon,inv_icon)=>{
-    let text=document.querySelector(`#${item}`)
-    text.style.color='red';
-
-    let valid_icon=document.querySelector(`#${item}.${v_icon}`)
-    
-    valid_icon.style.display='hidden';
-
-    let invalid_icon=document.querySelector(`#${item}.${inv_icon}`)
-    invalid_icon.style.display='inline'
+ },[formErrors])
+ //handle if valid
+ const valid = (item, v_icon, inv_icon) => {
    
+    let text = document.querySelector(`#${item}`);
+    text.style.color = "green";
+    // #change
+  };
+  //handle if invalid
+  const invalid = (item, v_icon, inv_icon) => {
+    let text = document.querySelector(`#${item}`);
+    text.style.color = "red";
     
+    // #change
+  };
+  const handleChange=(e)=>{
+    console.log(e.target);
+    const{name,value}=e.target;
+    setFormValues({...formValues,[name]:value})
+    console.log(formValues)
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    setFormErrors(validate(formValues))
+    setSubmit(true)
+    
+  }
+
+  const validate=(values)=>{
+
+    const errors={};
+    // const regex=/^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/
+    if(!values.username){
+
+        errors.username='Username is required!'
+        
+    }
+    if(!values.email){
+        errors.email='Email is required!'
+
+    }
+    if(!values.password){
+        errors.password='Password is required!'
+
+    }
+    return errors;
+
 
   }
+
+ 
   //handle input
   const handleInputChange=e=>{
     const password=(e.target.value);
     
     if(password.match(/[A-Z]/)!=null ){
+        setValidCapital(true)
         valid('capital','faCheck','faCross')
     }else{
+        setValidCapital(false)
         invalid('capital','faCheck','faCross')
     }
     if(password.match(/[a-z]/)!= null){
+        setValidSmall(true)
         valid('small','faCheck','faCross')
     }else{
+        setValidSmall(false)
         invalid('small','faCheck','faCross')
     }
     
     if(password.match(/[0-9]/)!= null){
+        setValidNumber(true)
         valid('number','faCheck','faCross')
     }
     else{
+        setValidNumber(false)
         invalid('number','faCheck','faCross') 
     }
-    if(password.length()>8){
+    if(password.length>8){
+        setValidPasswordlength(true)
         valid('length','faCheck','faCross')
     }else{
+        setValidPasswordlength(false)
         invalid('length','faCheck','faCross')
     }
     if(password.match(/[!@#$%^&*]/)!=null){
+        setValidSpecial(true)
         valid('specialChar','faCheck','faCross')
 
     }
     else{
+        setValidSpecial(false)
         invalid('specialChar','faCheck','faCross')
 
     }
@@ -81,57 +130,74 @@ const Form = () => {
   }
     return (
         <div >
-            <h1 style={{color:'lightgrey'}}>Start Your 15 Days Free Trial</h1>
+            
             <div className='form-body'>
-            <form action="" className='form'>
+            <form action="" className='form' onSubmit={handleSubmit}>
+            <h1 style={{color:'lightgrey'}}>Start Your 15 Days Free Trial</h1>
             <div>
-                <label htmlFor=""></label>
-                <input type="text" /></div>
+                
+               
+                <input type="text" placeholder='Username' name='username' value={formValues.username} onChange={handleChange} />
+                <p className='errors'>{formErrors.username}</p>
+                </div>
             <div>
-                <label htmlFor="">
-
-            </label>
-                <input type="text" /></div>
+                
+            
+                <input type="text" placeholder='Email' name='email' value={formValues.email} onChange={handleChange}/>
+                <p className='errors'>{formErrors.email}</p>
+                </div>
             <div>
-                <label htmlFor=""></label>
-                <input type={show?'text':'password'} placeholder='Password' className='password' onChange={handleInputChange} />
+                
+                <input type={show?'text':'password'} placeholder='Password' className='password' value={formValues.password}name='password'onChange={e=>{handleInputChange(e);handleChange(e)}} />
                 {
                     show?(<FontAwesomeIcon id='show-hide' icon={faEye} onClick={handleShowHide} />):(<FontAwesomeIcon id='show-hide' icon={faEyeSlash} onClick={handleShowHide} />)
                 }
+                <p className='errors'>{formErrors.password}</p>
                 </div>
-            </form>
+            
             <div className='validations'>
                 <div  id='status'>
-                <FontAwesomeIcon className='faCross icon' icon={faTimesCircle} />
-                <FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />
+                    {
+                        isValidPassword?(<FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />):(<FontAwesomeIcon className='faCross icon' icon={faTimesCircle}/>)
+                    }
+                 
+                
                    <span>Password status:</span> 
                     </div>
                 <div id='capital'>
-                <FontAwesomeIcon  className='faCross icon' icon={faTimesCircle} />
-                <FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />
+                {
+                        isValidCapital?(<FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />):(<FontAwesomeIcon className='faCross icon' icon={faTimesCircle}/>)
+                    }
                 <span> Atleast contain one Uppercase</span>
                 </div>
                 <div id='small'>
-                <FontAwesomeIcon className='faCross icon' icon={faTimesCircle} />
-                <FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />
+                {
+                        isValidSmall?(<FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />):(<FontAwesomeIcon className='faCross icon' icon={faTimesCircle}/>)
+                    }
                 <span>Atleast contain one lowercase</span>
                 </div>
                 <div id='specialChar'>
-                <FontAwesomeIcon className='faCross icon' icon={faTimesCircle} />
-                    <FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />
+                {
+                        isValidSpecial?(<FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />):(<FontAwesomeIcon className='faCross icon' icon={faTimesCircle}/>)
+                    }
                     <span>Atleast one special character</span>
                     </div>
                 <div id='number'>
-                <FontAwesomeIcon className='faCross icon' icon={faTimesCircle} />
-                    <FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />
+                {
+                        isValidNumber?(<FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />):(<FontAwesomeIcon className='faCross icon' icon={faTimesCircle}/>)
+                    }
                     <span>Atleast one number</span>
                     </div>
                 <div id='length'>
-                <FontAwesomeIcon className='faCross icon' icon={faTimesCircle} />
-                    <FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />
+                {
+                        isValidPasswordlength?(<FontAwesomeIcon className='faCheck icon' icon={faCheckCircle} />):(<FontAwesomeIcon className='faCross icon' icon={faTimesCircle}/>)
+                    }
                     <span>Length should be more than 8</span>
                     </div>
             </div>
+
+            <input type="submit" name="submit" className='submit-button' value="Create Account" />
+            </form>
             </div>
         </div>
     );
